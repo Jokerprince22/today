ce35665e8b1129af612245359c279e601c58e07c
Jokerprince22 <prince.in2x@gmail.com>
2021-11-15 13:59:19 +0530

    project added

---
+++ src/org/sample/sampleProject.java
@@ -0,0 +1,11 @@
+package org.sample;
+
+public class sampleProject {
+	private void repositery() {
+		System.out.println(1);
+		System.out.println(2);
+		System.out.println(3);
+		System.out.println(4);
+	}
+
+}
