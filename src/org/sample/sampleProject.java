13a9f2ec1de6384475ec77e9e4f2e4453e91cbcf
Jokerprince22 <prince.in2x@gmail.com>
2021-11-16 10:08:32 +0530

    new Files Added

--- src/org/sample/sampleProject.java
+++ src/org/sample/sampleProject.java
@@ -1,13 +1,36 @@
 package org.sample;
 
+import java.awt.List;
+import java.util.ArrayList;
+import java.util.Set;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class sampleProject {
-	private void repositery() {
-		System.out.println(1);
-		System.out.println(2);
-		System.out.println(3);
-		System.out.println(4);
-		System.out.println(8);
-		System.out.println(9);
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Prince\\eclipse-workspace\\GitHub\\driver\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://www.amazon.in/");
+		String currentUrl2 = driver.getCurrentUrl();
+		System.out.println(currentUrl2);
+		String title2 = driver.getTitle();
+		System.out.println(title2);
+		driver.navigate().to("https://www.toolsqa.com/");
+		driver.navigate().back();
+		
+		
+		
+		
+		
+		
+//		Set<String> windowHandle = driver.getWindowHandles();
+//		System.out.println(windowHandle);
+//		ArrayList<String> list=new ArrayList<String>(windowHandle);
+//		driver.switchTo().window(list.get(0));
+//		driver.quit();
+//		
+		
 	}
 
 }
